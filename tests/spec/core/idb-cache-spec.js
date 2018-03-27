3c99263a0276dfb91d5b8245a612ad9f4861e7f6
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-03-27 09:13:43 +0530

    changes done

--- tests/spec/core/idb-cache-spec.js
+++ tests/spec/core/idb-cache-spec.js
@@ -1,26 +1,28 @@
 "use strict";
 
-describe("Core - Idb Cache", () => {
+fdescribe("Core - Idb Cache", () => {
   let idbCache;
   beforeAll(done => {
-    require(["core/idb-cache"], (IDBCache) => {
+    require(["core/idb-cache"], ({default: IDBCache}) => {
       idbCache = IDBCache;
       done();
     });
   });
 
-  describe("put() method", () => {
+  fdescribe("put() method", () => {
 
-    it("stores data in pairs of key and value", async () => {
+    fit("stores data in pairs of key and value", async () => {
+      console.log(idbCache);
       let IDBCache = new idbCache("test-cache", ["storeA", "storeB"]);
       await IDBCache.put("storeAKey", { prop: "pass store A" });
       await IDBCache.put("storeBKey", { prop: "pass store B" }, "storeB");
-      expect(IDBCache.has("storeA")).toBe(true);
-      expect(IDBCache.has("storeB")).toBe(true);
-      const storeAData = IDBCache.get("storeA");
-      const storeBData = IDBCache.get("storeB");
-      expect(storeAData.storeAKey.prop).toBe("pass store A");
-      expect(storeBData.storeBKey.prop).toBe("pass store B");
+      console.log(IDBCache);
+      expect(IDBCache.stores.includes("storeA")).toBe(true);
+      expect(IDBCache.stores.includes("storeB")).toBe(true);
+      const storeAData = await IDBCache.match("storeAKey", "storeA");
+      const storeBData = await IDBCache.match("storeBKey", "storeB");
+      expect(storeAData.prop).toBe("pass store A");
+      expect(storeBData.prop).toBe("pass store B");
     });
   })
 });
